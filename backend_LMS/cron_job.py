49361e7863c5915666f54045bc0f7e4bef1eb579
Your Name <amosmaina907@gmail.com>
2022-02-23 05:13:18 +0300

    APScheduler

---
+++ backend_LMS/cron_job.py
@@ -0,0 +1,10 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+# Main cronjob function.
+from mpesa_payments.cron import check_balance
+
+# Create an instance of scheduler and add function.
+scheduler = BlockingScheduler()
+scheduler.add_job(check_balance, "interval", seconds=60)
+
+scheduler.start()
